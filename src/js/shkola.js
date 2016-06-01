a304f1030ffb2845c1ea2c54f4040a378b11c35b
Admin <code@tut.by>
2016-06-02 02:27:06 +0300

    insert html sections as view modules

--- src/js/shkola.js
+++ src/js/shkola.js
@@ -1,15 +1,35 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
-require('../main.less');
-//require("!style!css!less!../main.less");
+
+//require('font-awesome-webpack');
+
+require ('../less/variables.less');
+require ('../less/freelancer.less');
+
+//require ('../font-awesome/less/font-awesome.less');
+//require("font-awesome-webpack");
+
+import HeaderView from './view/HeaderView';
+import PortfolioView from './view/PortfolioView';
+import AboutView from './view/AboutView';
+import ContactView from './view/ContactsView';
+import FooterView from './view/FooterView';
+
+
 
 class App extends Component {
     render() {
-        return <div className="box">
+        return <div>
             <div>
-                <p> 123!</p>
+                <HeaderView />
+                <PortfolioView />
+                <AboutView />
+                <ContactView />
+                <FooterView />
             </div>
         </div>;
+        // <body id="page-top" className="index">123
+        // </body>
     }
 }
 
