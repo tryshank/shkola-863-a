43bfb5ef09aa81f89f9523e0c61d3ce8cc668f00
Admin <code@tut.by>
2016-06-06 22:53:42 +0300

    code reformat

--- src/js/shkola.js
+++ src/js/shkola.js
@@ -1,17 +1,15 @@
-import React, {Component} from 'react';
-import ReactDOM from 'react-dom';
+import React, {Component} from "react";
+import ReactDOM from "react-dom";
+import HeaderView from "./view/HeaderView";
+import PortfolioView from "./view/PortfolioView";
+import AboutView from "./view/AboutView";
+import ContactView from "./view/ContactsView";
+import FooterView from "./view/FooterView";
+import PortfolioModalView from "./view/PortfolioModalView";
 
 require('font-awesome/less/font-awesome.less');
-require ('../less/variables.less');
-require ('../less/freelancer.less');
-
-import NavigationView from './view/NavigationView';
-import HeaderView from './view/HeaderView';
-import PortfolioView from './view/PortfolioView';
-import AboutView from './view/AboutView';
-import ContactView from './view/ContactsView';
-import FooterView from './view/FooterView';
-import PortfolioModalView from './view/PortfolioModalView';
+require('../less/variables.less');
+require('../less/freelancer.less');
 
 
 var portfolioDataJSON = [
--- src/js/view/HeaderView.js
+++ src/js/view/HeaderView.js
@@ -1,17 +1,17 @@
-import React,{Component} from 'react';
+import React, {Component} from "react";
 export default class HeaderViewView extends Component {
 
-    render () {
+    render() {
         return <header>
             <div className="container">
                 <div className="row">
                     <div className="col-lg-12">
-                        <img className="img-responsive" src="src/img/profile.png" alt="" />
-                            <div className="intro-text">
-                                <span className="name">Start Bootstrap</span>
-                                <hr className="star-light" />
-                                    <span className="skills">Web Developer - Graphic Artist - User Experience Designer</span>
-                            </div>
+                        <img className="img-responsive" src="src/img/profile.png" alt=""/>
+                        <div className="intro-text">
+                            <span className="name">Start Bootstrap</span>
+                            <hr className="star-light"/>
+                            <span className="skills">Web Developer - Graphic Artist - User Experience Designer</span>
+                        </div>
                     </div>
                 </div>
             </div>
