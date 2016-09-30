400c5fe2b647a62bbebfa4a0da49f6d5539b661c
codeBlr <code@tut.by>
2016-09-30 19:06:39 +0300

    include set-env-vars.sh into dev-server.sh

--- dev-server.sh
+++ dev-server.sh
@@ -1,7 +1,4 @@
 #!/usr/bin/env bash
-echo "set vars..."
-export ENV_REDIS_SECRET="hey you"
-export ENV_SERVER_PORT=3000
-export ENV_MONGODB_URI="mongodb://localhost:27017/shkola"
+. ./set-env-vars.sh
 
 webpack-dev-server --hot --inline & cd server && NODE_ENV=development node app.js
