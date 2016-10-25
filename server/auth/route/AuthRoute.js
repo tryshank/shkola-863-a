0432af67a6ceaa20ac146380e00405369298920c
codeBlr <code@tut.by>
2016-10-26 01:18:34 +0300

    refactor auth/register route password check

--- server/auth/route/AuthRoute.js
+++ server/auth/route/AuthRoute.js
@@ -10,29 +10,33 @@ router.post('/login', passport.authenticate('local'), (req, res) => {
   res.status(200).send('logged in');
 });
 
-router.post('/register', (req, res) => {
-  if (req.body.adminPassword === process.env.REGISTRATION_ADMIN_PASSWORD) {
-    const userModel = new UserModel({ username: req.body.username });
-    UserModel.register(
-      userModel,
-      req.body.password,
-      (err, account) => {
-        if (err) {
-          res.status(401).send(err);
-        } else {
-          passport.authenticate('local')(
-            req,
-            res,
-            () => {
-              res.redirect('/admin');
-            }
-          );
-        }
-      }
-    );
+const checkPassword = (req, res, next) => {
+  if (req.body.adminPassword === process.env.ENV_REGISTRATION_ADMIN_PASSWORD) {
+    next();
   } else {
     res.status(401).end();
   }
+};
+
+router.post('/register', checkPassword, (req, res) => {
+  const userModel = new UserModel({ username: req.body.username });
+  UserModel.register(
+    userModel,
+    req.body.password,
+    (err, account) => {
+      if (err) {
+        res.status(401).send(err);
+      } else {
+        passport.authenticate('local')(
+          req,
+          res,
+          () => {
+            res.redirect('/admin');
+          }
+        );
+      }
+    }
+  );
 });
 
 router.get('/isLoggedIn', ensureAuthenticated);
--- set-env-vars.sh
+++ set-env-vars.sh
@@ -4,3 +4,4 @@ export ENV_MONGO_DEBUG="true"
 export ENV_REDIS_SECRET="hey you"
 export ENV_SERVER_PORT=3000
 export ENV_MONGODB_URI="mongodb://localhost:27017/shkola"
+export ENV_REGISTRATION_ADMIN_PASSWORD="1"
